fe5a707c4a4d3dc2bfc036ce923ea055c2286089
marius <marius.vorster@britehouse.co.za>
2018-03-18 12:18:57 +0200

    More files neeeded for testing

---
+++ Spike.SDK.Tests/EnumWorkerTests.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Spike.SDK.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using General.Enums;
+    using Component = System.ComponentModel;
+
+    public enum TestEnum
+    {
+        [Component.Description("None")]
+        Undefined = 0,
+
+        [Component.Description("First Seletion")]
+        SelectionOne =1,
+
+        [Component.Description("Second Selection")]
+        SelectionTwo =2,
+
+        [Component.Description("Third Selection")]
+        SelectionThree =3
+    }
+
+    [TestClass]
+    public class EnumWorkerTests
+    {
+        [TestMethod]
+        public void GetEnumDescriptionValue()
+        {
+            var listOfOptions = TestEnum.SelectionThree.ToDescription();
+
+            Assert.AreEqual("Third Selection", listOfOptions);
+        }
+
+        [TestMethod]
+        public void GetEnumValueFromString()
+        {
+            var thirdOption = EnumWorker.ParseEnum<TestEnum>("Third Selection");
+
+            Assert.AreEqual(TestEnum.SelectionThree, thirdOption);
+        }
+    }
+}
---
+++ Spike.SDK.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Spike.SDK.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Spike.SDK.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4a675ca-8043-4f10-a2a1-f06119d0b356")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
