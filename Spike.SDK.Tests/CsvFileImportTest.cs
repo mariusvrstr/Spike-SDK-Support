d3b1ff40466e8719cfb408e045f8de4a3702a0f3
marius <marius.vrstr@gmail.com>
2018-10-04 18:21:40 +0200

    Added Import Test Class

---
+++ Spike.SDK.Tests/CsvFileImportTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Spike.SDK.Tests
+{
+    [TestClass]
+    public class CsvFileImportTest
+    {
+        /* Start: Required structures for an import */
+
+        public enum CsvColumns
+        {
+            Name = 0,
+            Surname = 1
+        }
+
+        public class CsvLineItem
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+        }
+
+        /* End: Required structures for an import */
+
+
+        [TestMethod]
+        public void GenerateSampleFile()
+        {
+        }
+
+        [TestMethod]
+        public void ImportCsvFile()
+        {
+        }
+
+
+    }
+}
